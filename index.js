fb7cd4940cf99b48234ff13cea45d843e7ec0a24
Eliot Howes <ejhowes@gmail.com>
2019-04-28 16:02:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var i = 0;
   while (i < 3) {
-    facts[i] = facts[i].concat("!!!");
+    facts[i] = facts[].concat("!!!");
     i++;
   }
   return facts
